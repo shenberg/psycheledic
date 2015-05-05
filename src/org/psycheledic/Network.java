ec8eead68923100d5578e0413be3e6645a5dd0cb
Shay Erov <shay@contactspls.com>
2015-05-05 15:00:40 +0300

    Fixed broadcast to accept Ack

--- src/org/psycheledic/Network.java
+++ src/org/psycheledic/Network.java
@@ -5,14 +5,18 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashSet;
 
 public class Network {
 
     private static final int PORT = 5000;
     public static final String BROADCAST_IP = "255.255.255.255";
     public static final String[] IPS = {
-//            "192.168.137.202",
-            "192.168.137.201"
+            "192.168.137.201",
+            "192.168.137.202"
+//            "192.168.137.203",
+//            "192.168.137.204"
+//            "192.168.137.205"
 //            "10.0.0.70"
     };
     public static final short MAX_LEN = 1024;
@@ -45,19 +49,17 @@ public class Network {
             DatagramPacket datagramPacket = new DatagramPacket(data, data.length, addr, PORT);
             socket.send(datagramPacket);
             if (ip.equals(Network.BROADCAST_IP)) {
+                HashSet<String> acks = new HashSet<String>();
+                while (acks.size() < Network.IPS.length) {
+                    String ackIp = getAck(data[1]);
+                    System.out.println("Got ack for " + data[1] + " from " + ackIp);
+                    if (ackIp!=null) {
+                        acks.add(ackIp);
+                    }
+                }
                 return true;
-            }
-            byte[] recData = new byte[1024];
-            DatagramPacket recPacket = new DatagramPacket(recData, 1024);
-            socket.receive(recPacket);
-            String str = "" + recPacket.getLength() + " / ";
-            recData = recPacket.getData();
-            for (int i = 0; i < recPacket.getLength(); i++) {
-                str += recData[i] + " ";
-            }
-            Utils.debug(str);
-            if (recData[1] == data[1] && recData[0] == PacketType.ACK.ordinal()) {
-                return true;
+            } else {
+                return (("/"+ip).equalsIgnoreCase(getAck(data[1])));
             }
         } catch (IOException e) {
             // do nothing
@@ -68,4 +70,21 @@ public class Network {
         return false;
     }
 
+    private String getAck(byte seq) throws IOException {
+        byte[] recData = new byte[1024];
+        DatagramPacket recPacket = new DatagramPacket(recData, 1024);
+        socket.receive(recPacket);
+        recData = recPacket.getData();
+        if (Utils.DEBUG) {
+            String str = "" + recPacket.getLength() + " / ";
+            for (int i = 0; i < recPacket.getLength(); i++) {
+                str += recData[i] + " ";
+            }
+            Utils.debug(str);
+        }
+        if (recData[1] == seq && recData[0] == PacketType.ACK.ordinal()) {
+            return recPacket.getAddress().toString();
+        }
+        return null;
+    }
 }
